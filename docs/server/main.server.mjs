670fe5c78cde3567616df7bf72ae7b3c0e375403
Ahmed_Elswelky <aelswelky@aidotparse.com>
2024-12-13 14:44:28 +0200

    deoply

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-DE4RJYA6.mjs";import"./chunk-CA2YHYKT.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-5OT4DRAB.mjs";import"./chunk-CA2YHYKT.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
